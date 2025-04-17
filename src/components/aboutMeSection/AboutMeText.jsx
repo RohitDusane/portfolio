94de70c79646c0083e94922a0fd992e9b2118fad
Rohit Dusane <stat.data247@gmail.com>
2025-04-17 16:16:01 +0530

    Update AboutMeText.jsx

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -47,7 +47,7 @@ const AboutMeText = () => {
         {/* Resume Button */}
         <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
           <a
-            href="./public/resume.pdf" // Correct path to resume.pdf in the deployed folder
+            href="/portfolio-website/public/resume.pdf" // Correct path to resume.pdf in the deployed folder
             target="_blank"
             rel="noopener noreferrer"
             className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
