a54cf5952abc5f2db02b0fca6028140f247e3813
RohitDusane <stat.data247@gmail.com>
2025-04-18 14:34:36 +0530

    uptodate

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from 'tailwindcss';
+
+// export default defineConfig({
+//   base:"/website/",
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss],
+//     },
+//   },
+//   assetsInclude: ['**/*.pdf'],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  base:"/website/",
+  base: "/website/",
   plugins: [react()],
-  assetsInclude: ['**/*.pdf'], 
   css: {
     postcss: {
       plugins: [tailwindcss],
     },
   },
+  assetsInclude: ['**/*.pdf'], // Ensure PDF files are handled correctly
 });
