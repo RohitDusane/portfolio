288e9202b067d75d4737c3dd15dbec45240b4c56
RohitDusane <stat.data247@gmail.com>
2025-04-18 10:11:46 +0530

    assest added

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
   plugins: [react()],
   base:"/portfolio-website/",
-  build: {
-    outDir: 'dist',  // This is the default output directory, which is what gh-pages expects
-  },
   css: {
     postcss: {
       plugins: [tailwindcss],
