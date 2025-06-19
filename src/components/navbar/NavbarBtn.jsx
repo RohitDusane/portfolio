b1ab9fe09922fa53d0c83dbe8d202d474bc3a548
RohitDusane <stat.data247@gmail.com>
2025-06-19 15:15:08 +0530

    updated contact me button

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,12 +3,13 @@ import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
   return (
-    <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-darkCyan to-orange transition-all duration-300 hover:scale-105 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
+     <button className="w-fit h-fit
+     rounded-full font-bold text-white border-cyan border flex items-center gap-5 bg-gradient-to-r from-darkCyan to-orange transition-all duration-300 hover:scale-105 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
       <Link
         spy={true}
         smooth={true}
         duration={500}
-        offset={-120} // Adjust offset if needed to properly align the section on scroll
+        offset={-50} // Adjust offset if needed to properly align the section on scroll
         to="contact" // Targeting the Contact Me section
       >
         Contact Me
